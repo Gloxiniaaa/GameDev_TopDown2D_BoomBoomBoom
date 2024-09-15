4688c93ed338cdda9723f08c038f10ea346df87b
Gloxiniaaa <hnm142758@gamil.com>
2024-09-15 13:37:21 +0700

    event channel with scriptable object

---
+++ Assets/Scripts/EventListener/VoidEventListener.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// A flexible handler for void events in the form of a MonoBehaviour. Responses can be connected directly from the Inspector.
+/// but the reponse action(s) need to be public
+/// </summary>
+
+public class VoidEventListener : MonoBehaviour
+{
+	[SerializeField] private VoidEventChannelSO _channel = default;
+
+	public UnityEvent Response;
+
+	private void OnEnable()
+	{
+		if (_channel != null)
+			_channel.OnEventRaised += Respond;
+	}
+
+	private void OnDisable()
+	{
+		if (_channel != null)
+			_channel.OnEventRaised -= Respond;
+	}
+
+	private void Respond()
+	{
+		if (Response != null)
+			Response.Invoke();
+	}
+}
