43ddf8d963fb34e69832010028500eadc5f652d6
ThienVan1208 <ngvan12082005@gmail.com>
2024-09-17 12:47:07 +0700

    add start scene

---
+++ Assets/Scripts/ForStartScene/CharScene.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class CharScene : MonoBehaviour
+{
+     [SerializeField] private RectTransform _itself;
+    private Coroutine _curCo = null;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_curCo == null) {
+            _curCo = StartCoroutine(charEffect());
+        }
+    }
+    IEnumerator charEffect() {
+        _itself.DOAnchorPosY(155f, 1f);
+        yield return new WaitForSeconds(1f);
+        _itself.DOAnchorPosY(140f, 1f);
+        yield return new WaitForSeconds(1f);
+        _curCo = null;
+    }
+}
---
+++ Assets/Scripts/ForStartScene/EnemyScene.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class EnemyScene : MonoBehaviour
+{
+    [SerializeField] private RectTransform _itself;
+    private Coroutine _curCo = null;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_curCo == null) {
+            _curCo = StartCoroutine(enemyEffect());
+        }
+    }
+    IEnumerator enemyEffect() {
+        _itself.DOScaleY(3.8f, 1f);
+        yield return new WaitForSeconds(0.2f);
+        _itself.DOAnchorPos(new Vector2(73f, 25f), 0.8f);
+        yield return new WaitForSeconds(0.8f);
+
+        _itself.DOScaleY(3.2f, 1f);
+        yield return new WaitForSeconds(0.2f);
+        _itself.DOAnchorPos(new Vector2(79f, 35f), 0.8f);
+        yield return new WaitForSeconds(0.8f);
+
+        _curCo = null;
+    }
+}
