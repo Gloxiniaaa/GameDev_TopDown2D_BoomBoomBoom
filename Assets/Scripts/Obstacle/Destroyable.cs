5edae96b20b8b36b5c679ba49d42370d8f48c90a
Gloxiniaaa <hnm142758@gamil.com>
2024-09-17 23:31:25 +0700

    destroyable: spawn hidden item when destroyed

--- Assets/Scripts/Obstacle/Destroyable.cs
+++ Assets/Scripts/Obstacle/Destroyable.cs
@@ -2,5 +2,17 @@ using UnityEngine;
 
 public class Destroyable : MonoBehaviour
 {
-    
+    [Tooltip("leave it unassigned if u dont want this block to spawn an item after being destroyed")]
+    [SerializeField] private GameObject _hiddenItem;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag(Constant.ExplosionTag))
+        {
+            if (_hiddenItem)
+                Instantiate(_hiddenItem, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+
 }
