38e555b8fb211934b3d04954b6c0137134894ffd
ThienVan1208 <ngvan12082005@gmail.com>
2024-09-26 04:18:51 +0700

    nerf thunder enemy

--- Assets/Scripts/Enemy/JorgetEnemy.cs
+++ Assets/Scripts/Enemy/JorgetEnemy.cs
@@ -14,37 +14,44 @@ public class JorgetEnemy : EnemyBase
     private bool _isAttacking = false;
     private bool _isStop = false;
     private GameObject _player;
-    private void DetectPlayer() {
+    private void DetectPlayer()
+    {
         _player = GameObject.Find("Player");
     }
-    private IEnumerator Attack() {
-        
+    private IEnumerator Attack()
+    {
+
         float moveXDir = _rb.velocity.x;
         float moveYDir = _rb.velocity.y;
         _anim.SetTrigger(_atkTrigger);
         _anim.SetFloat(_attackX, moveXDir);
-        _anim.SetFloat(_attackY,moveYDir);
+        _anim.SetFloat(_attackY, moveYDir);
         _isStop = true;
         _rb.velocity = Vector3.zero;
-        foreach(var atk in _atkList) {
+        foreach (var atk in _atkList)
+        {
             Vector3Int newPlayerPos = _grid.WorldToCell(_player.transform.position);
             atk.transform.position = _grid.GetCellCenterWorld(newPlayerPos);
             atk.SetActive(true);
             yield return new WaitForSeconds(0.7f);
         }
         _isStop = false;
-       
+
     }
     private void OnEnable()
     {
+        DetectPlayer();
         _preAtk.SetActive(false);
-        foreach(var atk in _atkList) {
+        foreach (var atk in _atkList)
+        {
             atk.SetActive(false);
         }
         InvokeRepeating(nameof(AttackCounter), 1, _timeAttack);
     }
-    protected override void Update() {
-        if (!_isStop) {
+    protected override void Update()
+    {
+        if (!_isStop)
+        {
             base.Update();
         }
     }
@@ -64,25 +71,32 @@ public class JorgetEnemy : EnemyBase
     }
     protected override void Move(Vector3 newPos)
     {
-       if(_isAttacking) {
-        float dis = Vector2.Distance(transform.position, _nextPos);
-        if(dis <= 1.1f && dis >= 0.9f) {
-            _isAttacking = false;
-            if(_player == null) {
-                DetectPlayer();
+        if (_isAttacking)
+        {
+            float dis = Vector2.Distance(transform.position, _nextPos);
+            if (dis <= 1.1f && dis >= 0.9f && _player != null)
+            {
+                Vector3 atkDis = _player.transform.position - transform.position;
+                if (Mathf.Abs(atkDis.x) < _atkRange.x && Mathf.Abs(atkDis.y) < _atkRange.y)
+                {
+                    _isAttacking = false;
+                    StartCoroutine(Attack());
+                    
+                }
+                else base.Move(newPos);
             }
-            Debug.Log("atk");
-            StartCoroutine( Attack());
+            else
+            {
+                base.Move(newPos);
+            }
         }
-        else {
+        else
+        {
             base.Move(newPos);
         }
-       }
-       else {
-        base.Move(newPos);
-       }
     }
-    private void DisplayPreATK() {
+    private void DisplayPreATK()
+    {
         _preAtk.SetActive(true);
     }
 }
