28273541803d22754fe4fb9acd057a69991ad268
Gloxiniaaa <hnm142758@gamil.com>
2024-09-27 23:32:03 +0700

    destroyables: drops random item instead of 1 constant item

--- Assets/Scripts/Obstacle/Destroyable.cs
+++ Assets/Scripts/Obstacle/Destroyable.cs
@@ -1,23 +1,27 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Destroyable : MonoBehaviour
 {
     [Tooltip("leave it unassigned if u dont want this block to spawn an item after being destroyed")]
-    [SerializeField] private GameObject _hiddenItem;
+    [SerializeField] private List<GameObject> _hiddenItems;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag(Constant.ExplosionTag))
         {
-            if (_hiddenItem)
-                Instantiate(_hiddenItem, transform.position, Quaternion.identity);
+            if (_hiddenItems.Count != 0)
+            {
+                int i = Random.Range(0, _hiddenItems.Count);
+                Instantiate(_hiddenItems[i], transform.position, Quaternion.identity);
+            }
             Destroy(gameObject);
         }
     }
 
     private void OnDrawGizmos()
     {
-        if (_hiddenItem)
+        if (_hiddenItems.Count != 0)
         {
             Gizmos.color = Color.red;
             Gizmos.DrawLine(transform.position, transform.position + Vector3.up);
